5123588f0d9faa9fe545a2c704cdf388079af8ad
MingrenChen <chenmr9769@gmail.com>
2018-04-10 18:41:52 -0400

    final version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# MyDictionary
+# Nodejs web templete
+
+This project is a basic template for website, include
+a sign up/login page and a navbar which helps access
+login/sign up page easily. The databases.js connected to localhost
+mysql, and use passport js to validate login status.
+
+This template use localhost:3000 as default port, which you can
+change in main.js.
--- routes.js
+++ routes.js
@@ -33,7 +33,7 @@ module.exports = function(app, passport) {
         res.render('login.jade', { message: req.flash('loginMessage') });
     });
 
-    app.get('/signup', function (req, res) {
+    app.get('/signup', notLoggedIn, function (req, res) {
         res.render('signup.jade', { message: req.flash('signupMessage') })
     });
 
@@ -43,17 +43,6 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
 
-
-    app.get('/profile', isLoggedIn, function(req, res) {
-        Profile.findOne({email : req.user.local.email})
-            .then(function(doc){
-                console.log(doc);
-                res.render('profile.ejs',
-                    {user: req.user,
-                        profile: doc
-                    })
-            })
-    });
 
     // =====================================
     // LOGOUT ==============================
@@ -71,184 +60,6 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash message
         })
     );
-
-
-
-
-    app.post('/search', function(req, res){
-        res.render("search.ejs");
-    });
-
-    app.post('/addcountry', function(req, res){
-        new Profile({
-            email: req.user.local.email,
-            major: req.body.major,
-            StudentID : req.body.StudentID,
-            country : req.body.country,
-            name : req.body.name
-        }).save(function(err, doc){
-            if(err) {
-                res.redirect('/profile');
-            }
-
-            else res.redirect('/profile')
-
-        })
-    })
-
-//     app.post('/deleteC/', function(req, res){
-//         FavouriteCountry.findOneAndRemove({ uID: req.user.local.email, countryID: req.body.countryD}, function(err) {
-//           if (err) throw err;
-//         });
-//         res.redirect('/profile');
-//     })
-
-//     app.post('/editC/', function(req, res){
-//         FavouriteCountry.findOne({uID: req.user.local.email, countryID: req.body.countryE})
-//             .then(function(doc){
-//                 doc.comment= req.body.commentE;
-//                 doc.rate =req.body.ratingE;
-//                 doc.save();
-
-//         })
-//         res.redirect('/profile');
-//     })
-
-
-//     //TODO AJAX CALLLSZZZZ
-
-//     app.get('/RESTCOUNTRY/:country', function(req, res) {
-//         var country = req.params.country;
-//         console.log(country);
-//        request.get({ url: "https://restcountries.eu/rest/v2/name/" + country},      function(error, response, body) {
-//             if (!error && response.statusCode == 200) {
-//                 console.log('IN SERVER');
-
-//                 // console.log(body);
-//                 res.send(body).status(200);
-//             }
-//             else res.send(error);
-//         });
-
-//     });
-
-//     app.get('/ALLRESTCOUNTRY', function(req, res) {
-//         request.get({ url: "https://restcountries.eu/rest/v2/all"},      function(error, response, body) {
-//             if (!error && response.statusCode == 200) {
-//                 console.log('IN SERVER');
-
-//                 // console.log(body);
-//                 res.send(body).status(200);
-//             }
-//             else res.send(error);
-//         });
-
-//     });
-
-//     app.get('/EXCHANGE/:Ecode', function(req, res) {
-//         var Ec = req.params.Ecode;
-//         request.get({ url: 'http://api.fixer.io/latest?base=' + Ec},      function(error, response, body) {
-//             if (!error && response.statusCode == 200) {
-//                 console.log('IN SERVER');
-
-//                 // console.log(body);
-//                 res.send(body).status(200);
-//             }
-//             else res.send(error);
-//         });
-
-//     });
-
-
-
-//     //TODO handle messages
-//     // app.get('/api/messages', function(req, res) {
-//     //     Messages.find({}, function (err, messages) {
-//     //         res.json(messages).status(200);
-//     //     });
-//     // });
-
-
-//     app.get('/api/messages', function(req, res) {
-//         Messages.find({}, function (err, messages) {
-//             var mess = {};
-
-//             messages.forEach(function(message) {
-//                 mess[message._id] = message;
-//             });
-//             res.json(mess).status(200);
-//         });
-//     });
-
-//     var jsonParser = jsonParse()
-//     app.post('/api/messages',function(req, res) {
-//         // req.headers['content-type'] = 'application/json';
-//         // console.log(JSON.parse(req.body));
-//         console.log(req.headers['content-type']);
-//         var app = req.body.data;
-
-//         if (req.headers['content-type'] !== 'application/json') {
-//             for (key in req.body) {
-//                 app =JSON.parse(key).data;
-//             }
-//         }
-//         console.log('app    ===', app);
-//         var mes = new Messages({message : app});
-
-//         mes.save(function (err, saved) {
-//             if (err) throw err;
-//             res.status(200).json({
-//                 message: saved
-//             })
-
-//         })
-
-//     });
-
-//     app.delete('/api/messages/:num', function(req, res) {
-//         var idd = req.params.num;
-
-//         Messages.findByIdAndRemove(idd, function(err, todo) {
-//             // We'll create a simple object to send back with a message and the id of the document that was removed
-//             // You can really do this however you want, though.
-
-
-
-//             if (!todo) {
-//                 return res.status(404).json({success: false, msg: 'message not found'});
-//             }
-
-//             else if (!err) {
-//                 var response = {
-//                     message: "Todo successfully deleted",
-//                     id: todo._id
-//                 };
-//             }
-
-//             else {
-//                 var response = {
-//                     message: "ID IS WRONG, PLEASE ENTER THE CORRECT ID",
-
-//                 };
-//             }
-//             res.status(200).send(response);
-
-
-//         });
-//     });
-
-//     app.delete('/api/allmessages', function(req, res) {
-//         Messages.remove({}, function(err) {
-//                 if (err) {
-//                     console.log(err)
-//                 } else {
-//                     res.status(200).send('success');
-//                 }
-//             }
-//         );
-//     });
-
-
 
 };
 
